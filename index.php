20a9e879b2de2fcd82e14d47cf0a867a7b1f6ac9
koichi-inoue <inoue.ko@gmail.com>
2018-10-05 20:52:27 +0900

    Edit content

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="ja">
+
   <head>
     <meta charset="utf-8">
     <title>ECHO SAMPLE</title>
   </head>
+
   <body>
-    <?php
-      echo "<h1>Server Information</h1>";
-      echo "<p>".date("Y-m-d H:i:s")."</p>";
-      echo "<br>";
-      echo phpinfo();
-    ?>
+
+    <header>
+      <h1>PHP Basic Sample 01</h1>
+    </header>
+
+    <article>
+      <?php
+        echo "<h1>Server Information</h1>";
+        echo "<p>".date("Y-m-d H:i:s")."</p>";
+        echo "<br>";
+        echo phpinfo();
+      ?>
+    </article>
+
+    <footer>
+      http://www.example.com
+    </footer>
+
   </body>
 </html>
